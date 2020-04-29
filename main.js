8748e45e2d655f0aef6ae94d44e297532fa1a883
Luca Mangiagalli <luca.mangiagalli@finconsgroup.it>
2020-04-29 13:11:39 +0200

    output only in "one"

--- main.js
+++ main.js
@@ -1,19 +1,20 @@
- function random(){
+ $( document ).ready(function random(){
 
 $.get('/nouns.txt', function(response) {
   console.log('starting response noun')
   window.words = response.split('\n');
-  console.log(window.words[0]);
-  document.getElementById("one").innerHTML = window.words[Math.floor(Math.random() * window.words.length)];
+  window.current_word = window.words[Math.floor(Math.random() * window.words.length)];
+  //document.getElementById("one").innerHTML = window.words[Math.floor(Math.random() * window.words.length)];
 })
-
-console.log(window.words[0]);
+  console.log(window.current_word);
 
 $.get('/aa.txt', function(response) {
   console.log('starting response adj')
   window.adj = response.split('\n');
-  console.log(window.adj[0]);
-  document.getElementById("two").innerHTML = window.adj[Math.floor(Math.random() * window.adj.length)];
+  window.current_adj = window.adj[Math.floor(Math.random() * window.adj.length)];
+  //document.getElementById("two").innerHTML = window.adj[Math.floor(Math.random() * window.adj.length)];
 })
-  console.log(window.adj[0]);
-}
+  console.log(window.current_adj);
+  
+  document.getElementById("one").innerHTML = window.current_word.concat(" ", window.current_adj)
+});
