c4aefd83f5254d5a3a4c6863dcc2b9824b473416
Luca Mangiagalli <luca.mangiagalli@finconsgroup.it>
2020-04-29 13:18:25 +0200

    improved logs

--- main.js
+++ main.js
@@ -4,17 +4,18 @@ $.get('/nouns.txt', function(response) {
   console.log('starting response noun')
   window.words = response.split('\n');
   window.current_word = window.words[Math.floor(Math.random() * window.words.length)];
+  console.log(window.current_word);
   //document.getElementById("one").innerHTML = window.words[Math.floor(Math.random() * window.words.length)];
 })
-  console.log(window.current_word);
+
 
 $.get('/aa.txt', function(response) {
   console.log('starting response adj')
   window.adj = response.split('\n');
   window.current_adj = window.adj[Math.floor(Math.random() * window.adj.length)];
+  console.log(window.current_adj);
   //document.getElementById("two").innerHTML = window.adj[Math.floor(Math.random() * window.adj.length)];
 })
-  console.log(window.current_adj);
 
   document.getElementById("one").innerHTML = window.current_word.concat(" ", window.current_adj)
 }
