def13dd65ea88feb5a222c39b2a2202612c07f33
Luca Mangiagalli <luca.mangiagalli@finconsgroup.it>
2020-04-29 13:00:31 +0200

    fixing

--- main.js
+++ main.js
@@ -1,22 +1,19 @@
  function random(){
 
-window.words = [];
-window.adj = [];
-
 $.get('/nouns.txt', function(response) {
   console.log('starting response noun')
   window.words = response.split('\n');
-  console.log(window.words[0])
+  console.log(window.words[0]);
   document.getElementById("one").innerHTML = window.words[Math.floor(Math.random() * window.words.length)];
 })
 
-console.log(window.words[0])
+console.log(window.words[0]);
 
 $.get('/aa.txt', function(response) {
   console.log('starting response adj')
   window.adj = response.split('\n');
-  console.log(window.adj[0])
+  console.log(window.adj[0]);
   document.getElementById("two").innerHTML = window.adj[Math.floor(Math.random() * window.adj.length)];
 })
-  console.log(window.adj[0])
+  console.log(window.adj[0]);
 }
