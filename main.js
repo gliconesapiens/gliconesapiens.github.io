0ec872e25df330e1501c5eb30d057571bd766050
gliconesapiens <64334166+gliconesapiens@users.noreply.github.com>
2020-04-29 10:46:55 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,24 @@
  function random(){
 
 var words = [];
+var adj = [];
 
-$.get('/aa.txt', function(response) {
-  console.log('starting response')
+$.get('/nouns.txt', function(response) {
+  console.log('starting response noun')
   words = response.split('\n');
   console.log(words[0])
   document.getElementById("one").innerHTML = words[Math.floor(Math.random() * words.length)];
 })
+  
+  console.log(words[0])
+  
+$.get('/aa.txt', function(response) {
+  console.log('starting response adj')
+  adj = response.split('\n');
+  console.log(words[0])
+  document.getElementById("two").innerHTML = adj[Math.floor(Math.random() * words.length)];
+})
+  
 
 
 }
